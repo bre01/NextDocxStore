a45be7d68e6957890cecabf8c75a4bb14d2d75cf
bre <bre@bres-MBP.lan>
2023-12-08 12:26:28 +0800

    the v0.1 version
    Can add patient, search patient
    go to  patinet page, and download tables
    and upload tables

--- src/app/api/patient/route.ts
+++ src/app/api/patient/route.ts
@@ -14,24 +14,35 @@ export async function GET(
     request: NextRequest,
 ){
     const query=request.nextUrl.searchParams.get("query")
+    console.log(query)
     const folderPath = path.join(process.cwd(), `/public/patients`);
     try {
         const files = await fsPromises.readdir(folderPath);
         const res=files.map((file) => {return{name:file,link:`/emptytables/${file}`}})
-        const filtered=files.filter(file=>fileterPatient(file,query!))
-        return new Response(JSON.stringify(filtered))
+        //const filtered=await files.filter(async file=>fileterPatient(file,query!))
+
+        const filterBool=await Promise.all(files.map(async file=>{return {file,filterB:await fileterPatient(file,query!)}}))
+        
+        
+        const filtered=filterBool.filter(file=>file.filterB==true).map(file=>file.file)
+
+        console.log("-----------")
+        console.log(filtered)
+        console.log("-----------")
+        return new Response(JSON.stringify({message:"success",filtered}))
       } 
     catch (err) {
         console.error(err);
+        return new Response(JSON.stringify({messge:"failed"}))
     } 
 }
 
 const fileterPatient=async (file:string,query:string)=>{
     
-    
     const newPath = path.join(process.cwd(), `/public/patients/`);
     const detail= await JSON.parse(await fsPromises.readFile(newPath+file, 'utf8'))
-    if(detail.name.includes(query) || detail.description.includes(query)){
+    if(detail.name.includes(query) ){
+        console.log("true")
         return true;
     }
     else{
--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -2,16 +2,25 @@
 import React, { useState } from "react";
 import type Patient from "interfaces/Patient"
 export default function Patient(){
-    const [patients,setPatients]=useState<Array<Patient>>();
+    const [patients,setPatients]=useState<Array<any>>();
     const [query,setQuery]=useState<string>("") 
     const onChangeHandle=(event:React.ChangeEvent<HTMLInputElement>)=>{
         setQuery(event.target.value)
     }
     const submitHandle=()=>{
-        fetch(`http://localhost:3000/patient?query=${query}`)
+        console.log(query)
+        fetch(`/api/patient?query=${query}`,{method:"GET"})
         .then(res=>res.json())
-        .then((data:Array<Patient>)=>{
-            setPatients(data)
+        .then((data)=>{
+            console.log(data)
+            if(data.message=="failed"){
+                alert("try agagin")
+            }
+            else{
+                console.log("suc")
+                setPatients(data.filtered)
+                console.log(patients)
+            }
         })
     } 
 
@@ -19,7 +28,8 @@ export default function Patient(){
         <>
             <input value={query} onChange={onChangeHandle} ></input>
             <button onClick={submitHandle}>submit </button>
-            {patients&&patients.length?patients.map((patient,index)=>{<div><div>{patient.Id}</div><div>{patient.name}</div></div>}):
+            {patients&&patients.length?
+            patients.map((patient,index)=>{return (<div key={index}>{patient}</div>)}):
             <div>no data</div>}
 
         
